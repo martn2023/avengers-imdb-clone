f17660148a15e28dd2dea69744f00d315ed29d24
Martin <42924628+martn2022@users.noreply.github.com>
2024-06-03 01:21:51 -0400

    App.js drafted, dummy component files placed in components folder

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavigationPaneComponent from './components/NavigationPaneComponent';
+import BrowseMoviesComponent from './components/BrowseMoviesComponent';
+import BrowseActorsComponent from './components/BrowseActorsComponent';
+import MovieDetailsComponent from './components/MovieDetailsComponent';
+import ActorDetailsComponent from './components/ActorDetailsComponent';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <NavigationPaneComponent />
+                <Switch>
+                    <Route exact path="/" component={BrowseMoviesComponent} />
+                    //Some redundancy, but intended
+                    <Route path="/browse/movies" component={BrowseMoviesComponent} />
+                    <Route path="/browse-actors" component={BrowseActorsComponent} />
+                    <Route path="/movie/:id" component={MovieDetailsComponent} />
+                    <Route path="/actor/:id" component={ActorDetailsComponent} />
+                    <Route path="*" component={() => <div>404 Not Found</div>} />
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
