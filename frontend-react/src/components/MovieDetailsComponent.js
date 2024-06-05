0b63ca315ae06af44b833cc5fdd92fcc7442fcd0
Martin <42924628+martn2022@users.noreply.github.com>
2024-06-05 04:50:47 -0400

    working on cosmetic consistency and graying out value words on details page

--- frontend-react/src/components/MovieDetailsComponent.js
+++ frontend-react/src/components/MovieDetailsComponent.js
@@ -1,55 +1,51 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useParams, Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import '../App.css'; // Import the CSS file
 
 function MovieDetailsComponent() {
     const { id } = useParams();
     const [movie, setMovie] = useState(null);
-    const [actors, setActors] = useState([]);
 
     useEffect(() => {
-        axios.get(`http://localhost:3000/movies/${id}`)
-            .then(response => {
-                setMovie(response.data.movie);
-                setActors(response.data.actors);
-            })
-            .catch(error => {
+        const fetchMovieDetails = async () => {
+            try {
+                const response = await axios.get(`http://localhost:3000/movies/${id}`);
+                setMovie(response.data);
+            } catch (error) {
                 console.error('Error fetching movie details:', error);
-            });
+            }
+        };
+
+        fetchMovieDetails();
     }, [id]);
 
-    if (!movie) {
-        return <div>Loading...</div>;
-    }
-
-    const formattedReleaseDate = new Date(movie.release_date).toLocaleDateString('en-US', {
-        year: 'numeric', month: 'long', day: 'numeric'
-    });
+    if (!movie) return <div>Loading...</div>;
 
     return (
         <div className="movieDetailsContainer">
             <div className="movieDetailsContent">
                 <div className="movieDetailsLeft">
                     <img
-                        src={`/images/movies/${movie.id}/movie_poster_${movie.id}.jpg`}
-                        alt={`${movie.title}`}
+                        src={`/images/movies/${movie.movie.id}/movie_poster_${movie.movie.id}.jpg`}
+                        alt={`${movie.movie.title} Poster`}
                         className="moviePosterDetails"
+                        onError={(e) => { e.target.onerror = null; e.target.src="/images/movies/movie_poster_placeholder_marvel.jpg"; }}
                     />
                 </div>
                 <div className="movieDetailsRight">
                     <div className="movieDetailsTop">
-                        <h1 className="movieTitle">{movie.title}</h1>
-                        <p className="movieDate">Released: {formattedReleaseDate}</p>
+                        <div className="detailsTitle">{movie.movie.title}</div>
+                        <div className="detailsDate">Released: {new Date(movie.movie.release_date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}</div>
                     </div>
                     <div className="movieDetailsBottom">
-                        <h2>Actors</h2>
+                        <div className="detailsTitle">Actors</div>
                         <ul>
-                            {actors.map(actor => (
+                            {movie.actors.map(actor => (
                                 <li key={actor.id}>
-                                    <Link to={`/actor_details/${actor.id}`} className="actorLink">
+                                    <a href={`/actor_details/${actor.id}`} className="actorLink">
                                         {actor.first_name} {actor.last_name}
-                                    </Link>
+                                    </a>
                                 </li>
                             ))}
                         </ul>
