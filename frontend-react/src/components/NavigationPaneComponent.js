d6dcbf73be68d02b775d6e32d572603172454744
Martin <42924628+martn2022@users.noreply.github.com>
2024-06-04 22:46:27 -0400

    nav panel links moved 200 px to right by fixing CSS

--- frontend-react/src/components/NavigationPaneComponent.js
+++ frontend-react/src/components/NavigationPaneComponent.js
@@ -5,8 +5,10 @@ import '../App.css'; // Import the consolidated CSS file
 function NavigationPaneComponent() {
     return (
         <nav className="navigationContainer">
-            <Link to="/browse-movies" className="navigationLink">Browse Movies</Link>
-            <Link to="/browse-actors" className="navigationLink">Browse Actors</Link>
+            <div className="navigationLinks">
+                <Link to="/browse-movies" className="navigationLink">Browse Movies</Link>
+                <Link to="/browse-actors" className="navigationLink">Browse Actors</Link>
+            </div>
         </nav>
     );
 }
